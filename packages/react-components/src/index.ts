e76acbfb7a375182fe226139bd3ac80105803195
Artur Ribeiro <Artur.Carvalho@br.nestle.com>
2025-07-13 22:54:59 -0300

    first commit

---
+++ packages/react-components/src/index.ts
@@ -0,0 +1,5 @@
+import { palette } from '@art-design-system/tokens'
+
+console.log(palette.amber[500]);
+
+
---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,11 @@
+export * from './palette/index';
+
+export * from './fonts/index';
+export * from './fontSizes';
+export * from './fontWeights';
+export * from './lineHeights';
+
+export * from './radius';
+export * from './space';
+
+// TODO: criar tokens para: animations, shadows, z-indexes, breakpoints
