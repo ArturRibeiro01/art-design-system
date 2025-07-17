d682e7f467fb9dadf5518ba6358af5e94b7c3f80
Artur Ribeiro <Artur.Carvalho@br.nestle.com>
2025-07-17 00:57:15 -0300

    storybook init and themes init config

--- packages/docs/eslint.config.js
+++ packages/docs/eslint.config.js
@@ -1,26 +1,13 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import eslintConfig from '@art-design-system/eslint-config';
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    ...eslintConfig,
+    {
+        files: ['src/**/*.ts', 'src/**/*.tsx'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
     },
-  },
-], storybook.configs["flat/recommended"]);
+];
--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client"/>
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()]
+});
