bee0513c30191d661abdd720b58fb746965c921f
Artur Ribeiro <Artur.Carvalho@br.nestle.com>
2025-07-19 23:13:33 -0300

    feat não está funcionando

---
+++ packages/react-components/src/index.ts
@@ -0,0 +1,13 @@
+export { Button } from './Button/Button';
+
+// import { palette } from '@art-design-systems/tokens';
+//
+// console.log(palette.amber[500]);
+//
+// export function App() {
+//   return (
+//     <div style={{ backgroundColor: palette.green[200], padding: '20px', color: '#fff' }}>
+//       <h1>Welcome to the Art Design System</h1>
+//     </div>
+//   );
+// }
